23a77ae0fd892d98db0041be9244033d1bf87cc9
Victor <govs79@outlook.com>
2024-02-05 00:24:18 +0300

    Добавлен ввод числа

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int a = 161;
+﻿Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
 if (a%7 ==0 && a%23 == 0)
 {
     Console.WriteLine ($"{a} кратно одновременно 7 и 23");
